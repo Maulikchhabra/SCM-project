79d7ae3caa943ecb84158acc8dc63603957ca4ef
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2019-04-22 14:14:45 +0530

    Added some comments to the code

--- datastructure.cpp
+++ datastructure.cpp
@@ -3,6 +3,7 @@
 #include<cstdlib>
 //header files//
 using namespace std;
+//namespace//
 //--------------------------------------------------------------------------------------------------------//
 //variable declarations//
 int stack[100],n=100,top=-1;
@@ -14,39 +15,39 @@ int a, j, array[50], search1, first, last, middle;
 //Maulik starts//
 void stack1()//function 1 =stack implementation// 
 {
-	void push(int val);
-	void pop();
-	void display();
+	void push(int val); //to insert element//
+	void pop(); //to delete element//
+	void display(); //to display elements//
 	int num1, val,op;
 	        do 
 			{
 			   cout<<"STACK IMPLEMENTATION"<<endl;
-               cout<<"1) Push in stack"<<endl;
-               cout<<"2) Pop from stack"<<endl;
-               cout<<"3) Display stack"<<endl;
-               cout<<"4) Exit"<<endl;
-               cout<<"Enter choice: 1,2,3,4 "<<endl;
-               cout<<"\n";
-               cin>>num1;
+                           cout<<"1) Push in stack"<<endl;
+                           cout<<"2) Pop from stack"<<endl;
+                           cout<<"3) Display stack"<<endl;
+                           cout<<"4) Exit"<<endl;
+                           cout<<"Enter choice: 1,2,3,4 "<<endl;
+                           cout<<"\n";
+                           cin>>num1;
 			   switch(num1)
 			   {    
 			       case 1:cout<<"Enter value to be pushed:"<<endl;
-                           cin>>val;
-                           push(val);
-                           break;
-                   case 2:pop();
-                          break;
-                   case 3:display();
-                          break;  
-                   case 4:cout<<"Exit"<<endl;
-                          exit(1);
-                          break;
-                   default:cout<<"Invalid Choice"<<endl;
-                           break;
-		       }
-		       cout<<"press 0 to continue in stack : "<<endl;
-		       cin>>op;
-		   }   while(op==0);
+                                      cin>>val;
+                                      push(val);
+                                      break;
+                               case 2:pop();
+                                      break;
+                               case 3:display();
+                                      break;  
+                               case 4:cout<<"Exit"<<endl;
+                                      exit(1);
+                                      break;
+                              default:cout<<"Invalid Choice"<<endl;
+                                      break;
+		           }
+		           cout<<"press 0 to continue in stack : "<<endl;
+		           cin>>op;
+		         } while(op==0);
 }
 //-------------------------------------------------------------------------------------------------------------//
 void push(int val) {
@@ -88,33 +89,33 @@ void queue1()//function 2= queue implementation//
 {
 
 		int num2,op1;
-		void Enqueue();
-		void Dequeue();
-		void Display();
+		void Enqueue(); //to insert element//
+		void Dequeue(); //to delete element//
+		void Display(); //to display element//
 		do
 		{
 		    cout<<"QUEUE IMPLEMENTATION"<<endl;
-        	cout<<"1)Insert in the queue"<<endl;
-			cout<<"2)Delete from the queue"<<endl;
-			cout<<"3)Display the queue"<<endl;
-			cout<<"4)Exit"<<endl;
-			cout<<"Enter choice: "<<endl;
-			cin>>num2;
-            switch (num2)
+        	    cout<<"1)Insert in the queue"<<endl;
+	            cout<<"2)Delete from the queue"<<endl;
+	            cout<<"3)Display the queue"<<endl;
+		    cout<<"4)Exit"<<endl;
+		    cout<<"Enter choice: "<<endl;
+		    cin>>num2;
+                    switch (num2)
 		   {
-                case 1: Enqueue();
+                        case 1: Enqueue();
                         break;
-                case 2: Dequeue();
+                        case 2: Dequeue();
                         break;
-                case 3: Display();
+                        case 3: Display();
                         break;
-                case 4: cout<<"Exit"<<endl;
+                        case 4: cout<<"Exit"<<endl;
                         exit(1);
                         break;        
-            }
-            cout<<"press 1 to continue in queue:"<<endl;
-            cin>>op1;
-		 }while(op1==1);
+                }
+                cout<<"press 1 to continue in queue:"<<endl;
+                cin>>op1;
+              }while(op1==1);
 	}
 //-----------------------------------------------------------------------------------------------------------//
 void Enqueue()
